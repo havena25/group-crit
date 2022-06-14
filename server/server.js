601a9f201c86c0a1aefb45f64d1ad75353d32b13
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-14 20:38:18 +0100

    import 'path'

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require("express"); // import express
+const path = require("path"); // import path (to access file and directory paths)
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/api/", (req, res) => {
-  res.send("Hello!");
-});
+app.use(express.urlencoded({ extended: true })); // parse incoming string or array data
+app.use(express.json()); // parse incoming JSON data
 
 // connect to server
 app.listen(PORT, () => {
