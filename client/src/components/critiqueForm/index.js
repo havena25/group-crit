65646f01a8cd0d4d9cc320f3aac3181df6a5d7ba
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 14:44:00 +0100

    updated mutation hook

--- client/src/components/critiqueForm/index.js
+++ client/src/components/critiqueForm/index.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import { ADD_CRITIQUE } from "../../utils/mutations";
+import { QUERY_CRITIQUES, QUERY_ME } from "../../utils/queries";
 
 const CritiqueForm = () => {
   const [critiqueText, setText] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
+
+  const [addCritique, { error }] = useMutation(ADD_CRITIQUE, {
+    update(cache, { data: { addCritique } }) {
+      try {
+        // could potentially not exist yet, so wrap in a try...catch
+        const { Critique } = cache.readQuery({ query: QUERY_CRITIQUES });
+        cache.writeQuery({
+          query: QUERY_CRITIQUES,
+          data: { Critique: [addCritique, ...Critique] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, Critique: [...me.Critique, addCritique] } },
+      });
+    },
+  });
 
   const handleChange = (event) => {
     if (event.target.value.length <= 100000) {
