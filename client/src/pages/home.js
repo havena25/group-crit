492ee03906328c151c2c0d7c7b727de4ed22d4be
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 17:54:24 +0100

    update pages/home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
-import ArtList from "../components/artList";
-import CritiqueForm from "../components/CritiqueForm";
 
 import { QUERY_ARTWORKS } from "../utils/queries";
-import ArtForm from '../components/artForm';
-import { QUERY_ARTWORKS, QUERY_ME_BASIC } from "../utils/queries";
+
 import Auth from "../utils/auth";
 
+import ArtList from "../components/artList";
+// import ArtForm from "../components/artForm";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ARTWORKS);
   const artworks = data?.artworks || [];
+
   const loggedIn = Auth.loggedIn();
   return (
-
-    <main className="main_wrapper">
-          <div>
-            
-          </div>
-
-          <div>
+    <main>
+      <div>
+        <div>
           {loading ? <div>Loading...</div> : <ArtList artworks={artworks} />}
-          <CritiqueForm />
-          )}
         </div>
       </div>
     </main>
