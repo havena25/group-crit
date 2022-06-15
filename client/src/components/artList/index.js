16232d0ccec05b07266f80a0f1ec92cb1b5018b1
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 15:45:27 +0100

    style for artlist

--- client/src/components/artList/index.js
+++ client/src/components/artList/index.js
@@ -1,8 +1,53 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
 
-const ArtList = () => {
-  return <div>Test Art List</div>;
+const ArtList = ({ art }) => {
+  if (!art.length) {
+    return <h3>No art Yet</h3>;
+  }
+
+  return (
+    <div>
+      <div className="bg-dark">
+        <h2 className="english-font py-2 text-center fs-2 text-white">
+          Latest art
+        </h2>
+      </div>
+      <div>
+        {art &&
+          art.map((art) => (
+            <div key={art._id} className="py-3">
+              <h3>
+                <Link to={`/art/${art._id}`}>{art.artTitle}</Link>
+              </h3>
+              <p className="fs-6">
+                <strong>{art.createdAt}</strong> by{" "}
+                <strong>{art.username}</strong>
+              </p>
+              <div className="py-2">
+                <div>
+                  art Status: <strong>{art.artStatus}</strong>
+                </div>
+                <div>
+                  art Start Date: <strong>{art.artStartDate}</strong>
+                </div>
+              </div>
+              <div>
+                <p className="art-summary">art Summary: {art.artSummary}</p>
+                <p></p>
+                <p className="">
+                  <Link to={`/art/${art._id}`}>
+                    Critiques: {art.critiquetCount} || Click to{" "}
+                    {art.critiquetCount ? "see" : "start"} the discussion!
+                  </Link>
+                </p>
+                <div className="double-border"></div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ArtList;
