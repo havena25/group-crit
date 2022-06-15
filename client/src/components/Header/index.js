4b7e77e5492a4c1bd34dfa13d29677a362875128
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 15:47:47 +0100

    header styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header>
-      <Link to="/">
-        <h1>Group-crit</h1>
-      </Link>
-      <div className="double-border"></div>
-      <nav>
-        <div>Today's Date</div>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-      </nav>
-      <div className="double-border"></div>
+      <div>
+        <Link to="/" className="text-decoration-none text-body">
+          <h1>Group-crit</h1>
+        </Link>
+        <p>lorem</p>
+        <div className="double-border"></div>
+        <div className="d-flex flex-row justify-content-between">
+          <span className="english-font px-3 py-2">January 28th, 2021</span>
+          <nav className="d-flex flex-row justify-content-end">
+            {Auth.loggedIn() ? (
+              <>
+                {/* <Link to="/profile">Me</Link> */}
+                <a
+                  href="/"
+                  className="px-3 py-2 text-uppercase"
+                  onClick={logout}
+                >
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <Link to="/login" className="px-3 py-2 text-uppercase">
+                  Login
+                </Link>
+                <Link to="/signup" className="px-3 py-2 text-uppercase">
+                  Signup
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
+
+        <div className="double-border"></div>
+      </div>
     </header>
   );
 };
