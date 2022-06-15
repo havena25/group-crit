94b17ea0f92c1a1f3e580d25e61065de2148de2f
Alia Haven <aliahavenart@gmail.com>
2022-06-15 13:28:53 +0100

    more work on critiqueform

--- client/src/components/critiqueForm/index.js
+++ client/src/components/critiqueForm/index.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CritiqueForm = () => {
+
+  const [critiqueText, setText] = useState("");
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const handleChange = (event) => {
+    if (event.target.value.length <= 500) {
+      setText(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    setText("");
+    setCharacterCount(0);
+  };
+
   return (
     <div>
-      <p className="m-0">
-        Character Count: 0/280
-      </p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch">
+      <p className={`m-0 ${characterCount === 500 ? "text-error" : ""}`}>
+        Character Count: {characterCount}/500
+      </p>{" "}
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
         <textarea
           placeholder="Tell the artist what you think of their work"
+          value={critiqueText}
           className="form-input col-12 col-md-9"
+          onChange={handleChange}
         ></textarea>
         <button className="btn col-12 col-md-3" type="submit">
           Submit
