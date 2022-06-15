19f927e5f90b32dfd93c6ddcbfe5811d583dff24
Alia Haven <aliahavenart@gmail.com>
2022-06-15 12:48:40 +0100

    imports + queries

--- client/src/pages/singleArt.js
+++ client/src/pages/singleArt.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHT } from '../utils/queries';
+
+const SingleArt = (props) => {
+  const { id: artId } = useParams();
 
 const SingleArt = (props) => {
   return (
     <div>
       <div>
         <p className="card-header">
-          <span>Username</span> thought on createdAt
+          <span>Username</span> critique on createdAt
         </p>
         <div className="card-body">
-          <p>Case Text</p>
+          <p>Art Text</p>
         </div>
       </div>
     </div>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,40 @@
-import gql from 'graphql-tag';
+export const QUERY_CRITIQUES = gql`
+  query critiques($username: String) {
+    critiques(username: $username) {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+`;
+export const QUERY_PIECES = gql`
+  {
+    pieces {
+      _id
+      name
+      description
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_PIECE = gql`
+  query piece($id: ID!) {
+    piece(_id: $id) {
+      _id
+      pieceText
+      createdAt
+      username
+      critiqueCount
+      critiques {
+        _id
+        createdAt
+        username
+        critiqueBody
+      }
+    }
+  }
+`;
