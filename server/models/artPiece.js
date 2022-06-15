e6c7fafe96afde335d95b45a36f1307380d764b6
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 09:20:41 +0100

    adding more models + media model

--- server/models/artPiece.js
+++ server/models/artPiece.js
@@ -1,50 +1,53 @@
-const { Schema, model } = require('mongoose');
-const commentSchema = require('./Comment');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const commentSchema = require("./critque");
+const mediaSchema = require("./Media");
+const dateFormat = require("../utils/dateFormat");
 
-const artSchema = new Schema({
+const artSchema = new Schema(
+  {
     artTitle: {
-        type: String,
-        required: 'Your art piece or series should have a title',
-        minlength: 1,
-        maxlength: 120,
-        default: 'Untitled'
+      type: String,
+      required: "Your art piece or series should have a title",
+      minlength: 1,
+      maxlength: 120,
+      default: "Untitled",
     },
     artDescription: {
-        type: String,
-        required: 'Please describe your art piece or series',
-        minlength: 1,
-        maxlength: 500
+      type: String,
+      required: "Please describe your art piece or series",
+      minlength: 1,
+      maxlength: 500,
     },
     artStartDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     artStatus: {
-        type: String,
-        required: true,
-        default: 'WIP'
+      type: String,
+      required: true,
+      default: "WIP",
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: timestamp => dateFormat(timestamp)
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
     },
-    comments: [commentSchema]
-},
-{
+    media: [mediaSchema],
+    comments: [commentSchema],
+  },
+  {
     toJSON: {
-        getters: true
-    }
+      getters: true,
+    },
+  }
+);
+
+artSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
-artSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
-})
-
-
-const Art = model('Art', artSchema);
+const Art = model("Art", artSchema);
