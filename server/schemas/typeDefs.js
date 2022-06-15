68badf945f3ac2fc5fc54a457accca70a287f093
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-15 15:31:06 +0100

    typedefs and auth.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,51 +1,60 @@
 // imports
 const { gql } = require("apollo-server-express");
 
+// create our typeDefs
 const typeDefs = gql`
+  scalar DateTime
   type Art {
     _id: ID
     artTitle: String
+    artSummary: String
     artDescription: String
-    artStartDate: String
+    artStartDate: DateTime
     artStatus: String
     artAuthor: String
     createdAt: String
     username: String
+    critiqueCount: Int
+    critiques: [critique]
+  }
+  type Critique {
+    _id: ID
+    critiqueText: String
+    createdAt: String
+    username: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    art: [Art]
+    friends: [User]
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
   type Query {
-    artworks: [Art]
+    me: User
+    users: [User]
+    user(username: String!): User
+    art(username: String): [Art]
+    art(_id: ID!): art
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addArt(
+      artTitle: String!
+      artSummary: String!
+      artDescription: String!
+      artStartDate: String!
+      artStatus: String!
+    ): art
+    addCritique(artId: ID!, critiqueText: String!): art
+    addFriend(friendId: ID!): User
   }
 `;
-
-// // not finished yet
-// const typeDefs = gql`
-//   type Art {
-//     _id: ID
-//     artTitle: String
-//         artDescription: String
-//         artStartDate: String
-//         artStatus: String
-//         artAuthor: String
-//         createdAt: String
-//     username: String
-//   }
-//   type Query {
-//     artworks: [Art]
-//     user(username: String): User
-//   cases: [Case]
-//    case(_id: ID!): Case
-//   }
-//   type Mutation {
-//     login(email: String!, password: String!): Auth
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     addFriend(friendId: ID!): User
-//     addComment(commentText: String!): Comment
-//   }
-
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
-// `;
 
 module.exports = typeDefs;
