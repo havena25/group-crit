ba341c9421cd9f1488ffee5cfc9a080541441d00
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-16 20:26:06 +0100

    push

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
-        {location.pathname !== "/" && (
-          <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
-            &larr; Go Back
-          </button>
-        )}
         <h4>
           Made with{" "}
           <span
@@ -19,9 +11,7 @@ const Footer = () => {
             role="img"
             aria-label="heart"
             aria-hidden="false"
-          >
-            ❤️
-          </span>{" "}
+          ></span>{" "}
           loream loream
         </h4>
       </div>
