f9a60d40785258d9fc9bb71697a0a6c537ae45b2
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-16 20:20:40 +0100

    style changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
-    <footer>
-      <div>This is the footer</div>
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        {location.pathname !== "/" && (
+          <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
+            &larr; Go Back
+          </button>
+        )}
+        <h4>
+          Made with{" "}
+          <span
+            className="emoji"
+            role="img"
+            aria-label="heart"
+            aria-hidden="false"
+          >
+            ❤️
+          </span>{" "}
+          loream loream
+        </h4>
+      </div>
     </footer>
   );
 };
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,19 +3,27 @@ import { useQuery } from "@apollo/react-hooks";
 import ArtList from "../components/artList";
 // import CritiqueForm from "../components/CritiqueForm";
 
-// import ArtForm from '../components/artForm';
+import ArtForm from "../components/artForm";
 import { QUERY_ARTWORKS } from "../utils/queries";
-import Auth from "../utils/auth";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ARTWORKS);
   const artworks = data?.artworks || [];
-  const loggedIn = Auth.loggedIn();
   return (
     <main>
-      <div>
-        <div>
-          {loading ? <div>Loading...</div> : <ArtList artworks={artworks} />}
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          <ArtForm />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ArtList artworks={artworks} title="Some Feed for ArtWork" />
+          )}
         </div>
       </div>
     </main>
