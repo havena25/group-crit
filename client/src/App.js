33212cca1abb490ed6fffbfbe1540f653f5d8fbc
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-06-17 16:33:29 +0100

    working login

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 // import Apollo hooks and modules
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 // component import
 import Header from "./components/Header";
@@ -19,17 +25,26 @@ import Signup from "./pages/signup";
 import Profile from "./pages/Profile";
 import ArtForm from "./pages/artForm";
 
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
+const httpLink = createHttpLink({
   uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -44,8 +59,8 @@ function App() {
               <Route exact path="/" element={<Home />} />
               <Route exact path="/login" element={<Login />} />
               <Route exact path="/signup" element={<Signup />} />
-              <Route exact path="/art/:id" element={<SingleArt />} />
               <Route exact path="/profile/:username?" element={<Profile />} />
+              <Route exact path="/art/:id" element={<SingleArt />} />
               <Route exact path="/artform" element={<ArtForm />} />
               <Route element={<NoMatch />} />
             </Routes>
